ba5c920f07288edbabab52179b6d66e47ebc22b2
Realmzer <therealm4mc@gmail.com>
2024-08-24 08:02:05 -0400

    Update FreeplayState.hx

--- source/states/FreeplayState.hx
+++ source/states/FreeplayState.hx
@@ -163,7 +163,7 @@ class FreeplayState extends MusicBeatState
 
 		var leText:String = "Press SPACE to listen to the Song / Press CTRL to open the Gameplay Changers Menu / Press RESET to Reset your Score and Accuracy.";
 		bottomString = leText;
-		var size:Int = 24;
+		var size:Int = 20;
 		bottomText = new FlxText(bottomBG.x, bottomBG.y + 4, FlxG.width, leText, size);
 		bottomText.setFormat(Paths.font("vcr.ttf"), size, FlxColor.WHITE, CENTER);
 		bottomText.scrollFactor.set();
