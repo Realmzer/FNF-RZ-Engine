753d8a2c462a693ea9efa6283c8d8eaf594f5130
Andrew S <therealm4mc@gmail.com>
2024-09-22 07:32:25 -0400

    trying to fix workflow

--- project_files/setup/windows.bat
+++ project_files/setup/windows.bat
@@ -15,6 +15,7 @@ haxelib install hxdiscord_rpc 1.2.4
 haxelib install hxvlc 1.8.2
 haxelib install UnRAR
 haxelib install markdown
+haxelib install away3d
 haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit 1906c4a96f6bb6df66562b3f24c62f4c5bba14a7
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90
